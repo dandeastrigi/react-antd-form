775b489ed22865afd5011cff735375e59735fa42
Anderson Astrigi <dande.astrigi@gmail.com>
2020-05-04 14:03:06 -0300

    Form antd

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,14 @@ import React from 'react'
 import SignupForm from '../views/SignupForm'
 import AntdForm from '../views/AntdForm'
 import TopMenu from '../views/TopMenu'
+import UserMenu from '../views/UserForm'
 
 export default class App extends React.Component {
     render() {
         return (
             <div className="App">
                 <TopMenu />
-                <AntdForm/>
+                <UserMenu/>
             </div>
         )
     }
